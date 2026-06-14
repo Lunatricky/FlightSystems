da57ee42092c494502a9eabc61c631196b678bf7
Lunatricky <ender.localhost@gmail.com>
2026-06-14 19:53:20 +0100

    Partial fix to Sprited

--- Flight Systems/LCDInfo/Sprites.cs
+++ Flight Systems/LCDInfo/Sprites.cs
@@ -26,7 +26,7 @@ namespace IngameScript
             List<MySprite> sprites = new List<MySprite>();
 
             if (texts != null && texts.Count > 0)
-                sprites = BuildSprites(panel.SurfaceSize, cols, fontColor, backgroundColor);
+                sprites = BuildSprites(panel, cols, fontColor, backgroundColor);
 
             using (var frame = panel.DrawFrame())
             {
@@ -43,55 +43,64 @@ namespace IngameScript
             s.Position = center;
             s.Size = size;
             s.Color = backgroundColor;
-            s.Alignment = TextAlignment.CENTER;
+            s.Alignment = TextAlignment.LEFT;
             return s;
         }
 
         private static MySprite MakeTextSprite(Color fontColor, string text, Vector2 size, Vector2 labelPos, float scale)
         {
-            MySprite sprite = new MySprite();
-            sprite.Type = SpriteType.TEXT;
-            sprite.Data = text;
-            sprite.Position = labelPos;
-            sprite.Size = size;
-            sprite.Color = fontColor;
-            sprite.RotationOrScale = scale;
-            sprite.FontId = "DEBUG";
-            sprite.Alignment = TextAlignment.CENTER;
-            return sprite;
+            MySprite s = new MySprite();
+            s.Type = SpriteType.TEXT;
+            s.Data = text;
+            s.Position = labelPos;
+            s.Size = size;
+            s.Color = fontColor;
+            s.RotationOrScale = scale;
+            s.FontId = "DEBUG";
+            s.Alignment = TextAlignment.LEFT;
+            return s;
         }
 
         // Build sprites for a list of InfoItem
-        List<MySprite> BuildSprites(Vector2 panelSize, int cols, Color fontColor, Color backgroundColor)
-        {            
+        List<MySprite> BuildSprites(IMyTextSurface panel, int cols, Color fontColor, Color backgroundColor)
+        {
+            Vector2 surfaceSize = panel.SurfaceSize;
+            Vector2 textureSize = panel.TextureSize;
+
             var sprites = new List<MySprite>();
             // background
-            sprites.Add(MakeRectSprite(panelSize * 0.5f, panelSize, backgroundColor));
+            sprites.Add(MakeRectSprite(surfaceSize * 0.5f, surfaceSize, backgroundColor));
 
             int rows = texts.Count;
 
             float margin = 1f;
-            float innerW = panelSize.X - margin * (cols + 1);
-            float innerH = panelSize.Y - margin * (rows + 1);
+            float innerW = 20 + surfaceSize.X - margin * (cols + 1);
+            float innerH = surfaceSize.Y - margin * (rows + 1);
             float boxW = innerW / cols;
             float boxH = innerH / rows;
 
-            float incrmentY = 0f;
-            if (panelSize.X == 512f) incrmentY = 70f;
 
             for (int i = 0; i < rows; i++)
             {
                 int col = i % cols;
                 int row = i / cols;
-                float x = margin + col * (boxW + margin) + boxW * 0.5f;
-                float y = margin + row * (boxH + margin) + boxH * 0.5f;
-                var center = new Vector2(x, y + cols * incrmentY);
+                float x = col * (boxW + margin) + boxW * 0.5f;
+                float y = row * (boxH + margin) + boxH * 0.5f;
+
+                float posX = x / surfaceSize.X * textureSize.X;
+                float posY = y / surfaceSize.Y * textureSize.Y;
+
+
+                var center = new Vector2(posX, posY);
                 var size = new Vector2(boxW, boxH);
 
                 sprites.Add(MakeRectSprite(center, size, backgroundColor));
 
-                var labelPos = new Vector2(x - boxW * 0.38f, y - boxH * 0.25f + cols * incrmentY);
-                float scale = 1.9f - cols;
+                var labelPos = new Vector2(posX - boxW * 0.38f, posY - boxH * 0.25f + cols);
+                float scale;
+
+                if (surfaceSize.X > 512) scale = 1.5f;
+                else scale = 1.4f * surfaceSize.Y / 512f;
 
                 Color color;
 
--- Flight Systems/Program.cs
+++ Flight Systems/Program.cs
@@ -73,15 +73,6 @@ namespace IngameScript
 
         public void Main(string argument)
         {
-            foreach(IMyTextSurface surface in gc.Lcds1)
-            {
-                Echo("========");
-                Echo("Name       : " + surface.Name);
-                Echo("DisplayName: " + surface.DisplayName);
-                Echo("X: " + surface.SurfaceSize.X);
-                Echo("Y: " + surface.SurfaceSize.Y);
-            } 
-
             if (gc.ErrorMessage.Length > 0)
             {
                 Echo("ErrorMessage: \n" + gc.ErrorMessage.ToString());
@@ -288,9 +279,8 @@ namespace IngameScript
 
         public StringBuilder ScriptInfoHeader(StringBuilder scriptInfo)
         {
-            scriptInfo.AppendLine(gc.GridName);
-            scriptInfo.AppendLine(new string('-', 28));
-            scriptInfo.Append("State: " + command.State);
+            scriptInfo.AppendLine("Flight systems - " + gc.GridName);
+            scriptInfo.Append("    State: " + command.State);
 
             if (!string.IsNullOrEmpty(command.Param.Text))
                 scriptInfo.Append(" - " + command.Param.Text);
@@ -304,37 +294,36 @@ namespace IngameScript
 
         public StringBuilder ScriptInfoBlocks(IniContext ic, StringBuilder scriptInfo)
         {
-            scriptInfo.AppendLine();
             scriptInfo.AppendLine("Toggles");
-            scriptInfo.AppendLine(IniContext.FLIGHT_SYSTEMS + ": " + ic.AllowFlightSystems);
-            scriptInfo.AppendLine(IniContext.LOW_FUEL_LAND + ": " + ic.AllowLowFuelLand);
-            scriptInfo.AppendLine(IniContext.DOCK_MODE + ": " + ic.AllowDockMode);
-            scriptInfo.AppendLine(IniContext.CONTROL_ANTENNAS + ": " + ic.ControlAntennas);
-            scriptInfo.AppendLine(IniContext.RENAME_SUBGRIDS + ": " + ic.RenameSubgrids);
-            scriptInfo.AppendLine(IniContext.PAINT_SURFACES + ": " + ic.PaintSurfaces);
-            scriptInfo.AppendLine();
+            scriptInfo.AppendLine("    " + IniContext.FLIGHT_SYSTEMS + ": " + ic.AllowFlightSystems);
+            scriptInfo.AppendLine("    " + IniContext.LOW_FUEL_LAND + ": " + ic.AllowLowFuelLand);
+            scriptInfo.AppendLine("    " + IniContext.DOCK_MODE + ": " + ic.AllowDockMode);
+            scriptInfo.AppendLine("    " + IniContext.CONTROL_ANTENNAS + ": " + ic.ControlAntennas);
+            scriptInfo.AppendLine("    " + IniContext.RENAME_SUBGRIDS + ": " + ic.RenameSubgrids);
+            scriptInfo.AppendLine("    " + IniContext.PAINT_SURFACES + ": " + ic.PaintSurfaces);
             scriptInfo.AppendLine("Blocks");
-            scriptInfo.AppendLine("Controller: " + gc.Controller.CustomName);
+            scriptInfo.AppendLine("    Controller: " + gc.Controller.CustomName);
 
             if (ic.AllowFlightSystems)
             {
-                scriptInfo.AppendLine("Batteries: " + gc.Batteries.Count + " | Tanks: " + gc.Tanks.Count);
-                scriptInfo.AppendLine("Forward thruster: " + gc.ForwardThrusters.Count);
-                scriptInfo.AppendLine("Breaking thruster: " + gc.BreakingThrusters.Count);
-                scriptInfo.AppendLine("Upward thruster: " + gc.UpwardThrusters.Count);
-                scriptInfo.AppendLine("Gyros: " + gc.Gyros.Count);
+                scriptInfo.AppendLine("    Batteries: " + gc.Batteries.Count + " | Tanks: " + gc.Tanks.Count);
+                scriptInfo.AppendLine("    Forward thruster: " + gc.ForwardThrusters.Count);
+                scriptInfo.AppendLine("    Breaking thruster: " + gc.BreakingThrusters.Count);
+                scriptInfo.AppendLine("    Upward thruster: " + gc.UpwardThrusters.Count);
+                scriptInfo.AppendLine("    Gyros: " + gc.Gyros.Count);
             }
 
             if (ic.AllowDockMode || ic.AllowFlightSystems)
-                scriptInfo.AppendLine("Gears: " + gc.Gears.Count);
+                scriptInfo.AppendLine("    Gears: " + gc.Gears.Count);
 
             if (ic.AllowDockMode)
             {
-                scriptInfo.AppendLine("Dock Mode blocks: " + gc.ControlledBlocks.Count);
+                scriptInfo.AppendLine("    Dock Mode blocks: " + gc.ControlledBlocks.Count);
             }
 
-            scriptInfo.AppendLine("LCDs1: " + gc.Lcds1.Count);
-            scriptInfo.AppendLine("LCDs2: " + gc.Lcds2.Count);
+            scriptInfo.AppendLine("    LCDs1: " + gc.Lcds1.Count);
+            scriptInfo.AppendLine("    LCDs2: " + gc.Lcds2.Count);
+            scriptInfo.AppendLine("    Surfaces: " + gc.Surfaces.Count);
 
             return scriptInfo;
         }
@@ -598,6 +587,9 @@ namespace IngameScript
                     .ReloadH2Tanks()
                     .ReloadBatteries(ic.BackupBatteryTag);
 
+            GridContext.PaintSurfaces(ic, gc.Lcds1);
+            GridContext.PaintSurfaces(ic, gc.Lcds2);
+
             // Flight cached blocks
             if (ic.AllowFlightSystems)
             {
@@ -639,13 +631,7 @@ namespace IngameScript
             {
                 gc.ReloadSurfaces();
 
-                foreach(IMyTextSurface surface in gc.Surfaces)
-                {
-                    Color backgroundColor = ColorMap.GetColorFromString(ic.BackgroundColor);
-                    Color fontColor = ColorMap.GetColorFromString(ic.FontColor);
-
-                    GridContext.PaintSurface(surface, backgroundColor, fontColor);
-                }
+                GridContext.PaintSurfaces(ic, gc.Surfaces);
             }
         }
 
